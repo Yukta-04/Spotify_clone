1215c2de82711a0420713ae05cd6d72729684db5
Yukta-04 <73277684+Yukta-04@users.noreply.github.com>
2023-08-30 11:46:39 +0530

    Update spotify.js

--- spotify.js
+++ spotify.js
@@ -5,6 +5,8 @@ let masterPlay=document.getElementById('masterPlay');
 let myProgressBar=document.getElementById('myProgressBar');
 let gif=document.getElementById('gif');
 let displayName=document.getElementById('displayName');
+let currentTime=document.getElementById('currentTime')
+let totalTime=document.getElementById('totalTime');
 let songItems=Array.from(document.getElementsByClassName('songItem'));
 let songs=[
 {songName:"Hoshwalon Ko Khabar Kya", filePath:"sounds/1.mp3", coverPath:"image/mp1.jpg"},
@@ -38,10 +40,23 @@ masterPlay.addEventListener('click',()=>{
 
 })
 //Listen To Events
+function formatTime(minutes, seconds) {
+  const leadingZero = (value) => (value < 10 ? "0" + value : value);
+
+  const formattedMinutes = leadingZero(minutes);
+  const formattedSeconds = leadingZero(seconds);
+
+  return `${formattedMinutes}:${formattedSeconds}`;
+}
+
 
 audioElement.addEventListener('timeupdate',()=>{
-	console.log('timeupdate');
-	//update seekbar
+	const minutes = Math.floor(audioElement.currentTime / 60);
+	const seconds = Math.floor(audioElement.currentTime % 60);
+	currentTime.innerText = formatTime(minutes, seconds);
+	const min = Math.floor(audioElement.duration / 60);
+	const sec = Math.floor(audioElement.duration % 60);
+	totalTime.innerText = formatTime(min, sec);
 	progress=parseInt((audioElement.currentTime/audioElement.duration)*100);
 	myProgressBar.value=progress;
 });
@@ -52,6 +67,7 @@ myProgressBar.addEventListener('change',()=>{
 
 const makeAllPlays=()=>{
 	Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+		console.log('element')
 		element.classList.remove('fa-pause-circle');
 		element.classList.add('fa-play-circle');
 })
@@ -59,21 +75,23 @@ const makeAllPlays=()=>{
 Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
 
 	element.addEventListener('click',(e)=>{
+		console.log(e.target);
 		makeAllPlays();
 		index=parseInt(e.target.id);
-		console.log('e');
-		e.target.classList.remove('fa-play-circle');
-		e.target.classList.add('fa-pause-circle');
-		audioElement.src=`sounds/${index+1}.mp3`;
-		audioElement.currentTime=0
-		audioElement.play();
-		masterPlay.classList.remove('fa-play-circle');
-		masterPlay.classList.add('fa-pause-circle');
-		gif.style.opacity=1;
-		displayName.innerText=songs[index].songName;
-		
+			console.log(e.target.classList);
+			e.target.classList.remove('fa-play-circle');
+			e.target.classList.add('fa-pause-circle');
+			audioElement.src=`sounds/${index+1}.mp3`;
+			audioElement.currentTime=0;
+			audioElement.play();
+			masterPlay.classList.remove('fa-play-circle');
+			masterPlay.classList.add('fa-pause-circle');
+			gif.style.opacity=1;
+			displayName.innerText=songs[index].songName;
+
 
 	})
+
 })
 
 document.getElementById('next').addEventListener('click',()=>{
@@ -84,8 +102,12 @@ document.getElementById('next').addEventListener('click',()=>{
         index+=1;
 	}
 		makeAllPlays();
+		const elements = document.querySelectorAll('.songItemPlay');
+		const targetElement = elements[index];
+		targetElement.classList.remove('fa-play-circle');
+		targetElement.classList.add('fa-pause-circle');
 		audioElement.src=`sounds/${index+1}.mp3`;
-		audioElement.currentTime=0
+		audioElement.currentTime=0;
 		audioElement.play();
 		masterPlay.classList.remove('fa-play-circle');
 		masterPlay.classList.add('fa-pause-circle');
@@ -93,16 +115,20 @@ document.getElementById('next').addEventListener('click',()=>{
 		displayName.innerText=songs[index].songName;
 
 })
-document.getElementById('back').addEventListener('click',()=>{
+document.getElementById('back').addEventListener('click',(event)=>{
 	if(index<=0){
 		index=5;
 	}
 	else{
         index-=1;
 	}	
-		makeAllPlays();
+	makeAllPlays();
+		const elements = document.querySelectorAll('.songItemPlay');
+		const targetElement = elements[index];
+		targetElement.classList.remove('fa-play-circle');
+		targetElement.classList.add('fa-pause-circle');
 		audioElement.src=`sounds/${index+1}.mp3`;
-		audioElement.currentTime=0
+		audioElement.currentTime=0;
 		audioElement.play();
 		masterPlay.classList.remove('fa-play-circle');
 		masterPlay.classList.add('fa-pause-circle');
